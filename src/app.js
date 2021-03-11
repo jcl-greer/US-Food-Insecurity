edf34a747da7066aba44f92636c069f8237af08e
John Greer <j.c.greer12@gmail.com>
2021-03-10 20:59:32 -0600

    added a lil overshoot

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import {axisBottom, axisLeft} from 'd3-axis';
 import {format} from 'd3-format';
 import {symbol, symbolTriangle, line} from 'd3-shape';
 import {scaleQuantile} from 'd3-scale';
-import {transition, easeLinear} from 'd3-transition';
+import {transition} from 'd3-transition';
+import {ease, easeCubicIn, easeBounceOut, easeBackInOut} from 'd3-ease';
 import './main.css';
 import arrow1 from './charts/arrow1_trial';
 import arrow2 from './charts/arrow2_trial';
@@ -60,19 +61,62 @@ function myVis(initialData) {
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-  const t = transition().duration(300);
-  console.log('t is ', t);
-
+  // svg
+  //   .selectAll('.budget-scatter')
+  //   .data(data)
+  //   .join('circle')
+  //   .attr('class', 'budget-scatter')
+  //   .attr('cx', d => xScale(d[xDim]))
+  //   .attr('cy', d => yScale(d[yDim]))
+  //   .attr('r', 5)
+  //   .attr('fill', d => colorScale(d[colorVar]))
+  //   .attr('stroke', 'black');
+  const t = transition().duration(1500);
   svg
     .selectAll('.budget-scatter')
     .data(data)
-    .join('circle')
+    .join(enter =>
+      enter
+        .append('circle')
+        .attr('cy', d => yScale(d[yDim]) * 0)
+        .attr('cx', d => xScale(d[xDim]) * 2.5)
+        .call(el =>
+          el
+            .transition(t)
+            .ease(easeBackInOut.overshoot(1.5))
+            .attr('cy', d => yScale(d[yDim]))
+            .attr('cx', d => xScale(d[xDim])),
+        ),
+    )
     .attr('class', 'budget-scatter')
-    .attr('cx', d => xScale(d[xDim]))
-    .attr('cy', d => yScale(d[yDim]))
+
     .attr('r', 5)
     .attr('fill', d => colorScale(d[colorVar]))
     .attr('stroke', 'black');
+
+  // svg
+  // .selectAll('.circle')
+  // .data(data)
+
+  // .join(enter =>
+  //   enter
+  //     .append('circle')
+  //     .attr('cy', d => yScale(d[yDim]) * 0)
+  //     .attr('cx', d => xScale(d[xDim]) * 1.5)
+  //     .call(el =>
+  //       el
+  //         .transition(t)
+  //         .delay((d, i) => i * 0.5)
+  //         .attr('cy', d => yScale(d[yDim]))
+  //         .attr('cx', d => xScale(d[xDim])),
+  //     ),
+  // )
+  // .attr('class', 'circle')
+  // .filter(d => {
+  //   return d.Year === 2012;
+  // })
+  // .attr('r', 4)
+  // .attr('fill', '#1f77b4');
 
   svg
     .append('g')
@@ -108,7 +152,7 @@ function myVis(initialData) {
     .attr('y', 0 - margin.top / 2)
     .attr('font-size', 18)
     .text(
-      'State Food Insecurity Rates in 2012 Were Still High Due to the Great Recession',
+      'Large States Would Need to Spend a Lot More To Remedy Food Budget Shortfalls',
     );
   svg
     .append('g')
@@ -117,7 +161,7 @@ function myVis(initialData) {
     .attr('text-anchor', 'middle')
     .attr('x', plotWidth / 2)
     .attr('y', plotHeight + 30)
-    .text('Food Insecurity Rate')
+    .text('Total Annual Budget Shortfall ($)')
     .attr('font-size', 14);
 
   svg
