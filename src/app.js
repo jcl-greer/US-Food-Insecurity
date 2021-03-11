461271ce075cef4b2931c013ce1d91affdc77e75
John Greer <j.c.greer12@gmail.com>
2021-03-11 16:21:32 -0600

    added mouseout and tooltip but don't super understand what is happening there tbh

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {select, create} from 'd3-selection';
+import {select, create, pointer} from 'd3-selection';
 import {csv, json} from 'd3-fetch';
 import {scaleLinear, scaleTime, scaleBand} from 'd3-scale';
 import {extent, min, max, sum, range} from 'd3-array';
@@ -22,6 +22,13 @@ import arrow3 from './charts/arrow3_trial';
 
 // console.log('The new data is ', data);
 
+// good map stuff
+// https://observablehq.com/@d3/choropleth
+// https://observablehq.com/@d3/state-choropleth?collection=@d3/d3-geo
+
+// used this extensively for the tooltip
+// https://observablehq.com/@duynguyen1678/choropleth-with-tooltip
+
 Promise.all([
   json('./data/states-albers-10m.json'),
   json('./data/final_state_insecurity.json'),
@@ -37,14 +44,8 @@ Promise.all([
     console.log('the error is ', e);
   });
 
-// json('./data/states-albers-10m.json')
-//   .then(myVis)
-//   .catch(e => {
-//     console.log(e);
-//   });
-
 function myVis(us, insecure) {
-  console.log('starting this function');
+  console.log('starting this function', this);
   const height = 700;
   const width = 1200;
   const margin = {left: 100, top: 50, bottom: 50, right: 50};
@@ -56,10 +57,6 @@ function myVis(us, insecure) {
   // const yDim = 'Annual Food Budget Shortfall Per 100000';
   const colorDim = 'Food Insecurity Rate';
 
-  const color = scaleQuantize()
-    .domain([0.02, 0.35])
-    .range(schemeOrRd[9]);
-
   insecure = insecure.filter(d => d.Year === 2018);
 
   let data = insecure.reduce(
@@ -67,16 +64,12 @@ function myVis(us, insecure) {
     {},
   );
 
-  console.log('the object is ', data);
+  const color = scaleQuantize()
+    .domain(extent(insecure, d => d[colorDim]))
+    .range(schemeOrRd[9]);
 
-  // let data = Object.assign(
-  //   new Map(insecure),
-  //   ({state, rate}) => [state, +rate],
-  //   {
-  //     title: 'Unemployment rate (%)',
-  //   },
-  // );
-  console.log('data is ', insecure);
+  console.log('the object is ', data);
+  console.log('uncleaned (in js!) data is ', insecure);
   console.log('the us is ', us);
 
   let path = geoPath();
@@ -102,20 +95,105 @@ function myVis(us, insecure) {
 
   svg
     .append('g')
+    .attr('id', 'map')
     .selectAll('path')
     .data(topojson.feature(us, us.objects.states).features)
     .join('path')
     // .attr('fill', '#4280f4')
+    .attr('class', 'state')
     .attr('fill', d => color(data[d.id]))
-    .attr('d', path);
+    .attr('d', path)
+    .on('mouseover', function(d, i) {
+      console.log('this is ', this);
+      select(this)
+        .transition()
+        .duration('50')
+        .attr('opacity', '.5');
+    })
+    .on('mouseout', function(d, i) {
+      select(this)
+        .transition()
+        .duration('50')
+        .attr('opacity', '1');
+    });
 
   svg
+    .select('#map')
     .append('path')
     .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
     .attr('fill', 'none')
     .attr('stroke', 'white')
+    .attr('stroke-width', '1.5px')
     .attr('stroke-linejoin', 'round')
     .attr('d', path);
+
+  const callout = (g, value) => {
+    if (!value) return g.style('display', 'none');
+
+    g.style('display', null)
+      .style('pointer-events', 'none')
+      .style('font', '10px sans-serif');
+
+    const path = g
+      .selectAll('path')
+      .data([null])
+      .join('path')
+      .attr('fill', 'white')
+      .attr('stroke', 'black');
+
+    const text = g
+      .selectAll('text')
+      .data([null])
+      .join('text')
+      .call(text =>
+        text
+          .selectAll('tspan')
+          .data((value + '').split(/\n/))
+          .join('tspan')
+          .attr('x', 0)
+          .attr('font-size', '14px')
+          .attr('y', (d, i) => `${i * 1.1}em`)
+          .style('font-weight', (_, i) => (i ? null : 'bold'))
+          .text(d => d),
+      );
+
+    const {x, y, width: w, height: h} = text.node().getBBox();
+    console.log('this function was fired');
+
+    text.attr('transform', `translate(${-w / 2},${15 - y})`);
+    path.attr(
+      'd',
+      `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`,
+    );
+  };
+
+  const tooltip = svg.append('g');
+  console.log('this is ', this);
+  svg
+    .selectAll('.state')
+    .on('touchmove mousemove', function(event, d) {
+      tooltip.call(
+        callout,
+        `${d.properties.name}
+        ${colorDim} ${data[d.id]}`,
+      );
+      tooltip
+        .attr('transform', `translate(${pointer(event)})`)
+        .select('#map')
+        .attr('stroke', 'red')
+        .attr('font-size', '12px')
+        .raise();
+    })
+    .on('touchend mouseleave', function() {
+      tooltip
+        .call(callout, null)
+        .select('#map')
+        .attr('stroke', null)
+        .lower();
+    });
+
+  // return svg.node();
+
   // .on('click', d => {
   //   const node = svg.node();
   //   node.value = value = value === d.id ? null : d.id;
@@ -123,23 +201,6 @@ function myVis(us, insecure) {
   //   outline.attr('d', value ? path(d) : null);
   // });
 }
-// svg
-//   .append('path')
-//   .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-//   .attr('fill', 'none')
-//   .attr('stroke', 'white')
-//   .attr('stroke-linejoin', 'round')
-//   .attr('pointer-events', 'none')
-//   .attr('d', path);
-
-// const outline = svg
-//   .append('path')
-//   .attr('fill', 'none')
-//   .attr('stroke', 'black')
-//   .attr('stroke-linejoin', 'round')
-//   .attr('pointer-events', 'none');
-
-// return Object.assign(svg.node(), {value: null});
 
 // added a comment
 
